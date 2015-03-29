e5fd8ec5a2f78d2f897b91b008a1ffa5a825e5b7
Banshidhari Nandi <Banshi@Banshidharis-MacBook-Air.local>
2015-03-29 19:17:20 +0530

    - Forth Push

--- AutolayoutApp1/ViewController.m
+++ AutolayoutApp1/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"hi abp");
+    NSLog(@"hi abp World");
 }
 
 - (void)didReceiveMemoryWarning {
